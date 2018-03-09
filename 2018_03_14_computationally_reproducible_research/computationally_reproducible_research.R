fa172ec18254f8ded53c2e89e6cd673bfe6d5672
Wageningen University discussion group <r-users@wur.nl>
2018-03-09 17:21:54 +0100

    Update computationally_reproducible_research.R

--- 2018_03_14_computationally_reproducible_research/computationally_reproducible_research.R
+++ 2018_03_14_computationally_reproducible_research/computationally_reproducible_research.R
@@ -1,1 +1,25 @@
 
+sessionInfo()
+
+library(knitr)
+
+## run a .Rmd file in the Global environment
+knit('RMarkdown_example_1.Rmd')
+# don't forget to remove RMarkdown_example_1.md, and figure,
+# afterwards
+
+## Extract R code into .R file from .Rmd file
+purl('RMarkdown_example_1.Rmd')
+## and run it
+source('RMarkdown_example_1.R')
+
+
+## Create report from .R document with RMarkdown code 
+## behind 'roxygen' comments 
+library(rmarkdown)
+render(input = "RMarkdown_example_2.R",
+       output_format = "html_document", # other options: https://rmarkdown.rstudio.com/lesson-9.html
+       clean = TRUE  # removes temporarily files
+       )
+
+browseURL('RMarkdown_example_2.html')
